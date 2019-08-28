64bb60e2cfffc8c735387d5fb53de78286d65c80
Ray Sargolini <rsargolini@sbcglobal.net>
2019-08-28 13:01:45 -0400

    Finished Index Page

---
+++ public/scripts/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+//Connect Events to HTML Elements
+$(function ()
+{
+    // Get all data from API Leagues
+    $.getJSON("/api/leagues",
+        function (leagues)
+        {
+            let leaguesLength = leagues.length;
+
+            for (let i = 0; i < leaguesLength; i++)
+            {
+                // Dynamically create hockey divisions (Name and Description)
+                $("#hockeyDivisions").append($("<div>", {
+                    id: leagues[i].Code,
+                    class: "col-sm-3"
+                }))
+
+                $("#hockeyDivisions div:last").append($("<h2>", {
+                    text: leagues[i].Name,
+                }))
+
+                $("#hockeyDivisions div:last").append($("<p>", {
+                    text: leagues[i].Description,
+                }))
+            }
+        })
+})
