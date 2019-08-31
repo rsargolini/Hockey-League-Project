3e48793d66d5cad7f209273b000452c0e3983916
Ray Sargolini <rsargolini@sbcglobal.net>
2019-08-31 11:14:25 -0400

    Midday Commit

--- public/scripts/detailsteam.js
+++ public/scripts/detailsteam.js
@@ -27,7 +27,7 @@ function insertPlayerRow(teams, i)
 
 
     $("#players tbody tr:last td:last").append($("<a>", {
-        href: "detailsplayer.html?&playerid=" + teams.Members[i].MemberId,
+        href: "detailsplayer.html?&id=" + teams.TeamId  + "&playerid=" + teams.Members[i].MemberId,
         id: "editBtn" + [i],
         class: "btn btn-outline-success btn-sm",
         role: "button"
@@ -40,7 +40,6 @@ function insertPlayerRow(teams, i)
     $("#players tbody tr:last td:last").append($("<a>", {
         href: "deleteteam.html",
         id: "deleteBtn" + [i],
-        // text: "Delete",
         class: "btn btn-outline-danger btn-sm",
         role: "button"
     }))
@@ -227,9 +226,6 @@ $(function ()
             let objs = details;
 
             let leagues = JSON.parse(sessionStorage.getItem("leagues"));
-
-            console.log(details)
-            sessionStorage.setItem("teamData", JSON.stringify(details));
 
             let leaguesLength = leagues.length;
 
